b4f9bbec7ba0889d9657742706e2b0dec9d91995
CAngell0 <carsonjesseangell.learn@gmail.com>
2024-12-02 09:41:00 -0700

    Added Utilities.swift and haikyu pdf to project

---
+++ Creativity/ViewController/Utilites.swift
@@ -0,0 +1,14 @@
+//
+//  Utilites.swift
+//  Creativity
+//
+//  Created by Carson Angell on 12/2/24.
+//
+
+import Foundation
+
+//MARK: - URL's for documents
+
+let allHaiku = Bundle.main.url(forResource: "", withExtension: "pdf")
+
+//MARK: - Methods for...
