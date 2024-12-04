1b0d074f0a1426487ff98bc96732dc1ee4e1816b
CAngell0 <carsonjesseangell.learn@gmail.com>
2024-12-04 07:47:57 -0700

    Added ArtifactView and a sample artifact for it to use

--- Creativity/Resources/Utilites.swift
+++ Creativity/Resources/Utilites.swift
@@ -11,4 +11,8 @@ import Foundation
 
 let allHaiku = Bundle.main.url(forResource: "HaikuDocument", withExtension: "pdf")!
 
+//MARK: - Sample Artifact
+
+let sampleArtifact = Artifact(fileName: "Carson - Demo", title: "Mountain Artwork", description: "A piece of artwork I created a year ago.")
+
 //MARK: - Methods for...
---
+++ Creativity/ViewController/ArtifactView.swift
@@ -0,0 +1,18 @@
+//
+//  ArtifactView.swift
+//  Creativity
+//
+//  Created by Carson Angell on 12/4/24.
+//
+
+import SwiftUI
+
+struct ArtifactView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview ("Displayes an artifact"){
+    ArtifactView()
+}
