0990bcd1c9c45b6d9cc916d64109a51cba5bb360
CAngell0 <carsonjesseangell.learn@gmail.com>
2024-12-06 08:30:10 -0700

    Created a scribble method to draw a random scribbled shape

--- Creativity/ViewController/Drawings/ScribbleView.swift
+++ Creativity/ViewController/Drawings/ScribbleView.swift
@@ -15,7 +15,21 @@ struct ScribbleView: View {
 
 struct Scribble : Shape {
     private func drawScribble(within bounds : CGRect) -> Path {
-        let scribble = Path()
+        var scribble = Path()
+        
+        let startX = Int(arc4random()) % Int(bounds.midX)
+        let startY = Int(arc4random()) % Int(bounds.midY)
+        
+        scribble.move(to: CGPoint(x: startX, y: startY))
+        
+        for _ in 1...100 {
+            let randomX = Int(arc4random()) % Int(bounds.maxX);
+            let randomY = Int(arc4random()) % Int(bounds.maxY);
+            
+            scribble.addLine(to: CGPoint(x: randomX, y: randomY))
+        }
+        
+        scribble.closeSubpath()
         
         return scribble
     }
